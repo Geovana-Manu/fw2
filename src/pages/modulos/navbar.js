4f04ba028b853d0085b3e53b5591cc7fef3f0fc2
Geovana-Manu <120032533+Geovana-Manu@users.noreply.github.com>
2024-10-07 14:15:15 +0000

    aula 07/10

--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,4 +1,18 @@
-function Navbar() {
-    return <div><h2>Menu</h2></div>
+import {Container, Nav, Navbar} from 'react-bootstrap';
+
+export default function Menu() {
+  return (
+    <Navbar expand="sm" className="bg-secondary">
+      <Container>
+        <Navbar.Brand href="/">6A</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Produtos</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-export default Navbar;
